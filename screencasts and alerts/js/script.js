f125f2b4feca7e79af34a7e00044dc6c067e2977
russjr-fullsail <russjr@fullsail.edu>
2013-10-10 22:59:40 -0400

    promtps

--- screencasts and alerts/js/script.js
+++ screencasts and alerts/js/script.js
@@ -2,4 +2,9 @@
  * Created by russjr on 10/10/13.
  */
 
-prompt("Enter your year of birth")
+var userInput = prompt("Enter your year of birth");
+
+console.log(userInput);
+
+var width = prompt("We are calculating the area of a rectangle.  Please enter the height");
+var height = prompt("Please enter the height");
