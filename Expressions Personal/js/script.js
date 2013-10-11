f2780ddf8cf8caf162ad9835962e897970ae792d
russjr-fullsail <russjr@fullsail.edu>
2013-10-10 23:41:29 -0400

    personal

---
+++ Expressions Personal/js/script.js
@@ -0,0 +1,7 @@
+//Russell Chatham Jr. 10/10/13 Expressions Personal
+//dimensions of my first floor living room for carpet
+var width = 12.5;
+var length = 14.6;
+var areaLivingRoom = "The Living Room sq ft is\n" + width * length/2 + "sq feet";
+alert(areaLivingRoom);
+console.log(areaLivingRoom);
