4d80a1ed977a8b4d76eade96ff1566ad3a140683
russjr-fullsail <russjr@fullsail.edu>
2013-10-10 21:59:58 -0400

    oder of operations

--- expressions/js/script.js
+++ expressions/js/script.js
@@ -28,3 +28,14 @@ console.log(area);
 
 var remainder = 32 % 10;
 console.log(remainder)
+
+//Expressions - Order of Operation
+
+var quiz1 = 87;
+var quiz2 = 100;
+var quiz3 = 60;
+var quiz4 = 80;
+
+// the average is the sum of the items divided by the number of items
+var average = quiz1 + quiz2 + quiz3 + quiz4;
+console.log(average)
