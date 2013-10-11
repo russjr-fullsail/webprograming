506c020a921ba8c530a7ebf593c9cc18370251a9
russjr-fullsail <russjr@fullsail.edu>
2013-10-10 22:01:40 -0400

    updated order of operation

--- expressions/js/script.js
+++ expressions/js/script.js
@@ -37,5 +37,6 @@ var quiz3 = 60;
 var quiz4 = 80;
 
 // the average is the sum of the items divided by the number of items
-var average = quiz1 + quiz2 + quiz3 + quiz4;
+var average = (quiz1 + quiz2 + quiz3 + quiz4)/4;
+
 console.log(average)
