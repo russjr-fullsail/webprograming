493021639a8c7731cad4db40528f13db45de498a
russjr-fullsail <russjr@fullsail.edu>
2013-10-10 23:01:07 -0400

    area

--- screencasts and alerts/js/script.js
+++ screencasts and alerts/js/script.js
@@ -6,5 +6,7 @@ var userInput = prompt("Enter your year of birth");
 
 console.log(userInput);
 
-var width = prompt("We are calculating the area of a rectangle.  Please enter the height");
+var width = prompt("We are calculating the area of a rectangle. \nPlease enter the height");
 var height = prompt("Please enter the height");
+var area = width * height;
+console.log(area);
