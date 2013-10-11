545988429b9479a9a00cb654b2f4183b9198829b
russjr-fullsail <russjr@fullsail.edu>
2013-10-11 00:09:56 -0400

    industry -specific

--- Expressions Personal/js/script.js
+++ Expressions Personal/js/script.js
@@ -2,6 +2,6 @@
 //dimensions of my first floor living room for carpet
 var width = 12.5;
 var length = 14.6;
-var areaLivingRoom = "The Living Room sq ft is\n" + width * length/2 + "sq feet";
-alert(areaLivingRoom);
-console.log(areaLivingRoom);
+var area = "The Living Room sq ft is\n" + width * length/2 + "sq feet";
+alert(area);
+console.log(area);
---
+++ Industry-specific/js/script.js
@@ -0,0 +1,12 @@
+//Russell Chatham Jr. 10/10/13 Expressions - Industry-specific
+//Head Loss
+
+var g = 2; //g= Gravitational Constant
+var d = 5; //d= Pipe Diameter
+var l = 200; //l= Pipe Length
+var v = 3; //v= Flow Velocity
+var f = 0.019 //f= Friction Factor
+
+var headLoss = f * (l/d) * (v*2/2*g)
+console.log(headLoss);
+
