e781d13db0edfa0eb6f971cba1591049cbc94997
russjr-fullsail <russjr@fullsail.edu>
2013-10-15 21:09:28 -0400

    updated

--- Expression Worksheet/js/script.js
+++ Expression Worksheet/js/script.js
@@ -1,3 +1,8 @@
-/**
- * Created by russjr on 10/10/13.
- */
+//Russell Chatham Jr. 10/10/13 Expressions Worksheet
+
+//Dog Years
+
+var sparkysAge = 2;
+sparkysAge * 7;
+var dogYears = sparkysAge * dogYears;
+console.log(dogYears)
