dc41edf988046f78ee268842d0b1e5c3b2abd07d
russjr-fullsail <russjr@fullsail.edu>
2013-10-17 17:45:24 -0400

    Movie Ticket Prices

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -18,7 +18,7 @@ console.log("The temperature is", f + "", "degrees Fahrenheit");
 */
 //Check the Login
 
-var userInput = prompt ("Enter username");
+/*var userInput = prompt ("Enter username");
 var userInput2 = prompt ("Enter password");
 if(userInput == "Russ" && userInput2 == "webmaster"){
 console.log ("Welcome", userInput);
@@ -27,6 +27,24 @@ console.log ("Welcome", userInput);
 }
 if(userInput2 != "webmaster"){
     console.log("Password does not match our records");
+}*/
+
+//Movie Ticket Prices
+
+//movie ticket prices ar $12.00 but if you are over 55 or under 10 you get discount price of $7.00  If customer is seeing movie between 3 and 5 also get discounted ticket
+
+var age =31;
+var price;
+var time =3;
+
+if(price =(age >10 && age <55 || age <10 && age >55) ? "The ticket price is $12.00" : "The ticket price is $7.00"){
+console.log(price);
+}else{
+    (time == 3); "The ticket price is $7.00"
+    console.log(time)
+
 }
+
+
 
 
