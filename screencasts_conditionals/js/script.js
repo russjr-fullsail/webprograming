f455a97d8dcadeba482881a1e3ada6faa53310fe
russjr-fullsail <russjr@fullsail.edu>
2013-10-17 12:44:33 -0400

    added else if and var

--- screencasts_conditionals/js/script.js
+++ screencasts_conditionals/js/script.js
@@ -78,7 +78,7 @@ var sneakerLift = 2;
 if(kidHeight + sneakerLift > minHeight){
     //code performed if condition is true
     console.log("You can ride the coaster!");
-}**/
+}
 
 //If and else
 
@@ -92,5 +92,24 @@ if(kidHeight > minHeight){
     console.log("You can ride the coaster!");
 }else{
     //code performed if condition is false
+    console.log("Sorry kid, you've got some growing to do first!");
+}**/
+
+//Else if
+
+var kidHeight = 46;
+var minHeight = 48;
+var wParentHeight = 45;//the height of the kid with the parent
+//if the child is old enough, print to the console "you can ride!"
+//if the kid is over 48 inches in height
+
+if(kidHeight > minHeight){
+    //you can ride!
+    console.log("You can ride the coaster!");
+}else if(kidHeight > wParentHeight){
+    //you can ride with a parent present
+    console.log("You can ride, but only with a parent present");
+}else{
+    //sorry you have some growing to do
     console.log("Sorry kid, you've got some growing to do first!");
 }
