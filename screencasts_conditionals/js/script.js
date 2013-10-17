fccb0af5da61c8226a6bfe8ce81f259a209303b9
russjr-fullsail <russjr@fullsail.edu>
2013-10-17 00:05:28 -0400

    relations expressions

--- screencasts_conditionals/js/script.js
+++ screencasts_conditionals/js/script.js
@@ -43,7 +43,7 @@ if(sunny){
     }
 }else{
     goToTheMovies();
-}**/
+}
 
 //Conditionals Example
 
@@ -53,5 +53,18 @@ var oldEnough = true;
 if(oldEnough){
    //code performed if condition is true
     console.log("You can ride the coaster!");
+}**/
+
+//Relational Expressions
+
+var kidHeight = 40;
+var minHeight = 48;
+//if the child is old enough, print to the console "you can ride!"
+//if the kid is over 48 inches in height
+
+if(kidHeight > minHeight){
+    //code performed if condition is true
+    console.log("You can ride the coaster!");
 }
+
 
