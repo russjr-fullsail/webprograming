ceef456c467e7866b8191d40a95b7deed148f13e
russjr-fullsail <russjr@fullsail.edu>
2013-10-17 13:26:58 -0400

    fixed issue

--- screencasts_conditionals/js/script.js
+++ screencasts_conditionals/js/script.js
@@ -151,4 +151,4 @@ var book;
 //if the child is under 10, they get Green Eggs and Ham, otherwise they get The Time Machine
 book = (age <10) ? "Green Eggs and Ham" : "The Time Machine"
 console.log(book);
-*/
+
