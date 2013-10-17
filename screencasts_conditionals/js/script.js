a7b0ceb295857cc81583936f680c678d39d01042
russjr-fullsail <russjr@fullsail.edu>
2013-10-17 13:26:15 -0400

    changed age to see result

--- screencasts_conditionals/js/script.js
+++ screencasts_conditionals/js/script.js
@@ -143,7 +143,7 @@ var gpa = 48;
 //if the gpa is over the min 2.0 score, the student can graduate
 
 (gpa > 2.0) ? console.log("You can graduate!") : console.log("GPA too low!");
-*/
+
 
 var age = 8;
 var book;
@@ -151,3 +151,4 @@ var book;
 //if the child is under 10, they get Green Eggs and Ham, otherwise they get The Time Machine
 book = (age <10) ? "Green Eggs and Ham" : "The Time Machine"
 console.log(book);
+*/
