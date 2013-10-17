314c4c7fae71736815b715c9cd25b1360df1f80e
russjr-fullsail <russjr@fullsail.edu>
2013-10-17 17:25:12 -0400

    Check Login

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -1,7 +1,8 @@
 //Russell Chatham Jr. 10/17/13 Activity: Conditionals Worksheet
 
 //Celsius to Fahrenheit converter
-var userInput = prompt ("Please choose C or F")
+/*var userInput = prompt ("Please choose C or F")
+
 var userInput1 =("c");
 var userInput2 =("f");
 var f =32;
@@ -14,7 +15,18 @@ if(userInput1 = c){
 }
 console.log("The temperature is", c + "", "degrees Celsius");
 console.log("The temperature is", f + "", "degrees Fahrenheit");
-
+*/
 //Check the Login
+
+var userInput = prompt ("Enter username");
+var userInput2 = prompt ("Enter password");
+if(userInput == "Russ" && userInput2 == "webmaster"){
+console.log ("Welcome", userInput);
+}if(userInput != "Russ"){
+    console.log("User not found, Try again");
+}
+if(userInput2 != "webmaster"){
+    console.log("Password does not match our records");
+}
 
 
