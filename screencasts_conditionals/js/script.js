45d73adfad438f09ac7c60919dcf1f31ca6ed312
russjr-fullsail <russjr@fullsail.edu>
2013-10-17 13:32:09 -0400

    Conditionals Worksheet
    Start of the Conditionals Worksheet

--- screencasts_conditionals/js/script.js
+++ screencasts_conditionals/js/script.js
@@ -143,7 +143,7 @@ var gpa = 48;
 //if the gpa is over the min 2.0 score, the student can graduate
 
 (gpa > 2.0) ? console.log("You can graduate!") : console.log("GPA too low!");
-
+*/
 
 var age = 8;
 var book;
