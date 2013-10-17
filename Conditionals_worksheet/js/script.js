4baa3c364ddb4909243abf6754059c73cd7dabfb
russjr-fullsail <russjr@fullsail.edu>
2013-10-17 16:52:55 -0400

    celsius to farhenheit

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -1,14 +1,19 @@
 //Russell Chatham Jr. 10/17/13 Activity: Conditionals Worksheet
 
-//Miles to go
-
-var mpg = 18;
-var percent = "Gallons Left";
-var capacity = 22;
-
-miles = 200/mpg;
-
-console.log(miles);
+//Celsius to Fahrenheit converter
+var userInput = prompt ("Please choose C or F")
+var userInput1 =("c");
+var userInput2 =("f");
+var f =32;
+c=5/9*(f-32);
+var c =100;
+f= c * 9/5 + 32;
+if(userInput1 = c){
+}else{
+    (userInput2 = f)
+}
+console.log("The temperature is", c + "", "degrees Celsius");
+console.log("The temperature is", f + "", "degrees Fahrenheit");
 
 //Check the Login
 
