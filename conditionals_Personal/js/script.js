be302b79dd6d8d9926dcfc4d7845f28c5a5b1ed4
russjr-fullsail <russjr@fullsail.edu>
2013-10-17 21:15:06 -0400

    conditionals_personal
    i need to wait script

---
+++ conditionals_Personal/js/script.js
@@ -0,0 +1,20 @@
+//Russell Chatham Jr. 10/17/13 Conditionals Assignment - Conditionals_Personal
+
+//Can I buy new tires?
+
+var payCheck = 387;
+var weeksOfPay = 4;
+var checking = 500;
+var newTires = 3200;
+
+if(payCheck + checking > newTires){
+    console.log("Yes I can buy new tires!")
+}else if(payCheck * weeksOfPay > newTires){
+    //if I save money for all 4 weeks of pay can I get the tires?
+    console.log("Yes I can buy new tires")
+}else{
+    console.log("I need to wait longer for my new tires")
+}
+
+
+
