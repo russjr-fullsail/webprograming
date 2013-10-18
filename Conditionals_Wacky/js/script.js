6d4fac06b080b8bc3bbcc875412c71b0f82aad10
russjr-fullsail <russjr@fullsail.edu>
2013-10-17 22:11:01 -0400

    fixing code

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -3,9 +3,9 @@
 //Can I buy a new Jeep
 
 var jeepCost = 32000;
-var bankAccountTotal = prompt("Please enter how much is currently in your bank account:\n(Please enter as dollar value. i.e. 498.02)");
+var bankAccountTotal = prompt("Please enter how much is currently in your bank account:\n(Please enter as dollar value.)");
 var monthsUntilIBuy = prompt("Please enter how many months before you buy new Jeep:");
-var monthlyIncome = prompt("Please enter your monthly income :\n(Please enter as dollar value. i.e. 498.02)");
+var monthlyIncome = prompt("Please enter your monthly income :\n(Please enter as dollar value.)");
 
 //formula for calculating
 var checkUntilJeep = monthsUntilIBuy * monthlyIncome;
