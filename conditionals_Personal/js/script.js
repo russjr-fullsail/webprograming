ef24ba8ec49cc79f98880cee82279123b9a5bb3a
russjr-fullsail <russjr@fullsail.edu>
2013-10-17 21:17:32 -0400

    removed variable
    removed the variable for weeks of pay.

--- conditionals_Personal/js/script.js
+++ conditionals_Personal/js/script.js
@@ -3,15 +3,11 @@
 //Can I buy new tires?
 
 var payCheck = 387;
-var weeksOfPay = 4;
 var checking = 500;
 var newTires = 1200;
-
+//pay check plus checking account
 if(payCheck + checking > newTires){
     console.log("Yes I can buy new tires!")
-}else if(payCheck * weeksOfPay > newTires){
-    //if I save money for all 4 weeks of pay can I get the tires?
-    console.log("Yes I can buy new tires")
 }else{
     console.log("I need to wait longer for my new tires")
 }
