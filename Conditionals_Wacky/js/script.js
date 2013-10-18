2269879fa64e28be718ce2bebbb2013bed1a736c
russjr-fullsail <russjr@fullsail.edu>
2013-10-17 22:28:17 -0400

    setup user input prompts

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -1,1 +1,8 @@
 //Russell Chatham Jr 10/17/13 Conditionals Assignment - Conditionals Wacky
+
+//Can I buy a new Jeep
+//setup user input prompts
+var jeepCost = 32000;
+var bankAccountTotal = prompt("Please enter total in your bank account:\n(Please enter as dollar value.)");
+var monthsUntilIBuy = prompt("Please enter how many months before you buy new Jeep:");
+var monthlyIncome = prompt("Please enter your monthly income :\n(Please enter as dollar value.)");
