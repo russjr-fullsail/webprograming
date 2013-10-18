9eb80d2fcc2e5b95fc195e94715e7d0226e41950
russjr-fullsail <russjr@fullsail.edu>
2013-10-17 21:49:23 -0400

    fixing statements

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -3,10 +3,10 @@
 //Hours available to run my own business
 
 //hours
-var time = 30;
+var time = 8;
 var hoursInDay = 24;
 var daysAvailable = 7;
-var workHours = 9;
+var workHours = 4;
 var workDays = 5;
 var sleep = 4;
 
@@ -17,7 +17,7 @@ var work = (workHours * workDays);
 //if total hours are less than time, I can work on building my business
 if (sleepTime + work + hoursTotal < time){
     console.log("I have enough time to work on my business")
-}else if(time < hoursTotal + sleepTime){
+}else if(time > hoursTotal + sleepTime){
     console.log("I can sleep less to get the job done")
 }else{
     //I don't have enough time
