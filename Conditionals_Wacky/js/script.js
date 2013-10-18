ecdbc3453a269f51e9fc4f6d912cf3944c67eb91
russjr-fullsail <russjr@fullsail.edu>
2013-10-17 22:09:58 -0400

    setting up conditionals

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -2,7 +2,13 @@
 
 //Can I buy a new Jeep
 
-var jeepCost = 32,000;
+var jeepCost = 32000;
 var bankAccountTotal = prompt("Please enter how much is currently in your bank account:\n(Please enter as dollar value. i.e. 498.02)");
 var monthsUntilIBuy = prompt("Please enter how many months before you buy new Jeep:");
-var MonthlyIncome = prompt("Please enter your monthly income :\n(Please enter as dollar value. i.e. 498.02)");
+var monthlyIncome = prompt("Please enter your monthly income :\n(Please enter as dollar value. i.e. 498.02)");
+
+//formula for calculating
+var checkUntilJeep = monthsUntilIBuy * monthlyIncome;
+var checkPlusBankAccount = parseInt(bankAccountTotal) + parseInt(monthsUntilIBuy);
+
+//Using conditionals we can determine if a new Jeep is possible in the amount of time given
