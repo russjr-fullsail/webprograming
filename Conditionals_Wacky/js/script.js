259d43606e5bba0501298bca4db5dfedbbc6066a
russjr-fullsail <russjr@fullsail.edu>
2013-10-17 22:06:31 -0400

    start of project
    starting code

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -1,2 +1,8 @@
 //Russell Chatham Jr 10/17/13 Conditionals Assignment - Conditionals Wacky
 
+//Can I buy a new Jeep
+
+var jeepCost = 32,000;
+var bankAccountTotal = prompt("Please enter how much is currently in your bank account:\n(Please enter as dollar value. i.e. 498.02)");
+var monthsUntilIBuy = prompt("Please enter how many months before you buy new Jeep:");
+var MonthlyIncome = prompt("Please enter your monthly income :\n(Please enter as dollar value. i.e. 498.02)");
