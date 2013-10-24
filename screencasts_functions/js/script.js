9bd9aa207b9561623777d0046556007befffd18c
russjr-fullsail <russjr@fullsail.edu>
2013-10-23 22:06:17 -0400

    Function Invocation

--- screencasts_functions/js/script.js
+++ screencasts_functions/js/script.js
@@ -16,3 +16,20 @@ function calcArea(){
     var area = width * height;
     console.log(area);
 }
+
+//Function Invocation
+
+function outptMsg(){
+    console.log("Hello World");
+}
+
+function calcArea(){
+    var width = 20;
+    var height = 30;
+    var area = width * height;
+    console.log(area);
+}
+
+calcArea();
+calcArea();
+calcArea();
