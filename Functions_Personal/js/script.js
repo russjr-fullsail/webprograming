38052742bf8c7780c514cb6d3924dddded81c332
russjr-fullsail <russjr@fullsail.edu>
2013-10-24 18:57:16 -0400

    adding var

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -1,1 +1,11 @@
 //Russell Chatham Jr. 10/24/13 Functions Personal
+
+//Square Foot of upstairs rooms
+//room 1 width and length
+var widthRoom1 = prompt ("What is the width of Room 1");
+//enter width of room 1
+var lengthRoom1 = prompt ("What is the length of Room 1");
+//enter length of room 1
+var totalRoom1 = calcArea(widthRoom1, lengthRoom1);
+//total area of room 1
+
