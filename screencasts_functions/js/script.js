ebfb49db579842d82c938f082e8e8bab754d7f2f
russjr-fullsail <russjr@fullsail.edu>
2013-10-23 22:02:32 -0400

    Basic Structure

--- screencasts_functions/js/script.js
+++ screencasts_functions/js/script.js
@@ -2,4 +2,17 @@
 
 //Basic Function Structure
 
+function functionName(){
+    //code the function runs
+}
 
+function outptMsg(){
+    console.log("Hello World");
+}
+
+function calcArea(){
+    var width = 20;
+    var height = 30;
+    var area = width * height;
+    console.log(area);
+}
