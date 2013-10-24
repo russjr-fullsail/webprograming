b23ed0142c9fbd5237a16fea45d26c3f075997e6
russjr-fullsail <russjr@fullsail.edu>
2013-10-24 19:38:03 -0400

    trying to get if else if to work

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -2,18 +2,21 @@
 
 //How much money was made designing ads this month
 
-
     var adsDesigned = prompt("How many ads were designed this month");
     //input for how many ads were designed
 
-    var amountCharged = adsDesigned * 115;
+    var amountCharged = 125;
     //how many ads were designed * amount charged
 
-    console.log("I created " + adsDesigned + " this month and made a total of $" + amountCharged);
-function adsSold(){
-    if(amountCharged > 1000){
-        console.log("I had a great month designing ads")
-    }else if(amountCharged >500 && amountCharged < 999){
-        console.log("I need to try harder if I want to make this business work")
-    }
+function adsSold(adsDesigned, amountCharged){
+    var dollars = adsDesigned * amountCharged;
+return dollars;
+}
+
+console.log("I created " + adsDesigned + " this month and made a total of $" + amountCharged);
+
+if(amountCharged > 1000){
+    console.log("I had a great month designing ads");
+}else if(amountCharged >500 && amountCharged < 999){
+    console.log("I need to try harder if I want to make this business work");
 }
