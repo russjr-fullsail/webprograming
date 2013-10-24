628903c86b74b6e2efbc3f76d845827b5d425ca1
russjr-fullsail <russjr@fullsail.edu>
2013-10-23 22:09:47 -0400

    Variable Scope

--- screencasts_functions/js/script.js
+++ screencasts_functions/js/script.js
@@ -33,3 +33,16 @@ function calcArea(){
 calcArea();
 calcArea();
 calcArea();
+
+//Variable Scope
+
+var width = 5;
+function calcArea(){
+    var width = 20;
+    var height = 30;
+    var area = width * height
+    //console.log(area)
+}
+
+calcArea();
+console.log(width);
