2157fdb54a6b4bacb87ad0a49a5be576d1782821
russjr-fullsail <russjr@fullsail.edu>
2013-10-24 16:09:40 -0400

    adding comments

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -2,7 +2,7 @@
 
 //Ramp Travel Index Score or RTI Score
 
-var rti = calcTravel(35, 119)
+var rti = calcTravel(35, 119) //parameter1 is distance up ramp, parameter2 is wheelbase
 
 function calcTravel(dist, wheelBase){
 
@@ -10,6 +10,6 @@ function calcTravel(dist, wheelBase){
     return rti
     //Return RTI Score
 }
-console.log("This is your Ramp Travel Index " + rti + " score");
+console.log("This is your Ramp Travel Index " + rti + " score");//This is the result on the RTI Ramp
 
 
