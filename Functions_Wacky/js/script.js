7900a499c24b529d4d18688402a1a4dc515cd5d8
russjr-fullsail <russjr@fullsail.edu>
2013-10-24 15:59:37 -0400

    functions wacky
    workig on code for RTI score

---
+++ Functions_Wacky/js/script.js
@@ -0,0 +1,14 @@
+//Russell Chatham Jr. 10/24/13 Functions Wacky
+
+//Ramp Travel Index Score or RTI Score
+
+function calcTravel(dist, wheelBase){
+
+    var dist = 42; //dist = distance traveled up the ramp
+    var wheelBase= 119; //wheelBase = wheelbase of vehicle
+    var r; //r = calculated ramp travel index
+    return r
+}
+console.log("This is your Ramp Travel Index " + dist / wheelBase * 1000, + "score")
+
+
