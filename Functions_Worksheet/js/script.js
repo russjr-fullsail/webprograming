006647bab986a464ec7e8979e3d13db157ebaf00
russjr-fullsail <russjr@fullsail.edu>
2013-10-23 22:48:30 -0400

    Functions_Worksheet - Circumference
    Circumference problem

---
+++ Functions_Worksheet/js/script.js
@@ -0,0 +1,13 @@
+//Russell Chatham Jr. 10/23/13 Functions_Worksheet
+
+//Circumference - Calculate the circumference of a circle
+
+var radius = circleCircumference(3.14, 8)
+
+function circleCircumference(pi, dia){
+    var circumference = pi * dia;
+    return circumference
+    //Return Circumferenc
+}
+    console.log("The cicumference of the circle is " + radius)
+
